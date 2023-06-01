10b92509fc481b483dc35e0b46491b2722f48e33
zoya_rassadkinaO <zoya_rassadkina@epam.com>
2023-06-01 16:22:01 +0300

    add scripts

---
+++ scripts/build-client.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# --configuration=production to build in prod mode
+
+prod="--configuration=production"
+buildDirName="dist"
+zipFileName="client-app.zip"
+zipPath="$buildDirName/$zipFileName"
+
+npm install
+
+if [[ -f $zipPath ]]; then
+  rm $zipPath
+fi
+
+if [[ $1 == $prod ]]; then
+  echo 'Building production...'
+  npm run build $1
+else
+  npm run build
+fi
+
+zip -r $zipPath $buildDirName
+if [[ -f $zipPath ]]; then
+  echo 'Archive is created'
+fi
